55107d0e7633f2260195bf22c209842f573f9fe0
ivteplo <37793399+ivteplo@users.noreply.github.com>
2022-03-06 20:29:22 +0000

    Deploying to gh-pages from @ ivteplo/loyalty-cards-wallet@bb2bea3bbf36afc9869659430d40662b695d6668 🚀

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2022 Ivan Teplov
 
-const cacheName = "discountCardsWallet@1.1.1"
+const cacheName = "discountCardsWallet@1.1.2"
 const filesToCache = [
   "index.html",
   "global.css",
