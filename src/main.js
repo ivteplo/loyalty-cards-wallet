e3d9d5a40fc2324165bce377208913220882ee1d
Ivan Teplov <iteplov7@gmail.com>
2022-11-15 23:27:28 +0200

    Start working on improving the PWA
    I'm trying to use the vite-plugin-pwa instead of my custom service worker.
    It will fix the issue #6, but I'm still working on it

--- src/main.js
+++ src/main.js
@@ -9,22 +9,3 @@ const app = new App({
 })
 
 export default app
-
-function registerServiceWorker() {
-  if (!navigator.serviceWorker) return
-
-  console.log("navigator.serviceWorker is available")
-
-  navigator.serviceWorker
-    .register("worker.js", { scope: "." })
-    .then((registration) => {
-      console.log(
-        "[Service worker] Registered with the scope " + registration.scope
-      )
-    })
-    .catch(console.error)
-}
-
-if (process.env.NODE_ENV === "production") {
-  registerServiceWorker()
-}
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
+import { VitePWA } from "vite-plugin-pwa"
 import svelteSVG from "vite-plugin-svelte-svg"
 
 export default defineConfig({
@@ -12,5 +13,28 @@ export default defineConfig({
       requireSuffix: false
     }),
     svelte({}),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Loyalty Card Wallet",
+        short_name: "Wallet",
+        description: "One place for all of your loyalty cards",
+        background_color: "#fff",
+        theme_color: "#4d4bf1",
+        categories: ["finance", "shopping"],
+        icons: [
+          {
+            src: "icons/192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "icons/512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
   ]
 })
